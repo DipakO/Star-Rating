0f6365410d42d126d2817dc1881ba36c0ed1495d
Dipak Ohol <dipakohol070@gmail.com>
2022-11-13 13:37:05 +0530

    Star rating

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import Card from "./pages";
+import "./App.css"
+import Star from "./components";
+// import Card from "./components";
+const App =()=>{
+    return(
+        <div>
+            <Star />
+        </div>
+        
+    )
+    
+}
+
+export default App;
---
+++ src/raw/rawData.jsx
@@ -0,0 +1,9 @@
+<div className={number ==='1' ? "circle" : "square"}>
+
+</div>
+<div className={number ==='2'? "circle" : "square"}>
+
+</div>
+<div className={number ==='3' ? "circle" : "square"}>
+
+</div>
